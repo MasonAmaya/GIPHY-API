16f4bcc26e33046f8df8b626e720d85594825dea
MasonAmaya <32787009+MasonAmaya@users.noreply.github.com>
2018-02-01 14:52:12 -0700

    Add files via upload

---
+++ Giphy API/assets/main.js
@@ -0,0 +1,84 @@
+$(document).ready(function() {
+
+var topics = [];
+
+	function displayPokemon() {
+
+	var x = $(this).data("search");
+	console.log(x);
+
+	var queryURL = "https://api.giphy.com/v1/gifs/random?api_key" + x + "UH0M09IdKH66IzKPPfXqDx6rxZV6VmNW&tag=&rating=G";
+
+	console.log(queryURL);
+
+	$.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+        	var results = response.data;
+        	console.log(results);
+        	for (var i = 0; i < results.length; i++) {
+        	
+        	var showDiv = $("<div class='col-md-4'>");
+
+        	var rating = results[i].rating;
+        	var defaultAnimatedSrc = results[i].images.fixed_height.url;
+        	var staticSrc = results[i].images.fixed_height_still.url;
+        	var showImage = $("<img>");
+        	var p = $("<p>").text("Rating: " + rating);
+
+        	showImage.attr("src", staticSrc);
+        	showImage.addClass("pokemonGiphy");
+        	showImage.attr("data-state", "still");
+        	showImage.attr("data-still", staticSrc);
+        	showImage.attr("data-animate", defaultAnimatedSrc);
+        	showDiv.append(p);
+        	showDiv.append(showImage);
+        	$("#gifArea").prepend(showDiv);
+
+        }
+	});
+}
+
+  //Submit button click event takes search term from form input, trims and pushes to topics array, displays button
+	$("#addShow").on("click", function(event) {
+        event.preventDefault();
+        var newShow = $("#pokemonInput").val().trim();
+        topics.push(newShow);
+        console.log(topics);
+        $("#pokemonInput").val('');
+        displayButtons();
+      });
+
+  //Function iterates through topics array to display button with array values in "myButtons" section of HTML
+	function displayButtons() {
+    $("#myButtons").empty();
+    for (var i = 0; i < topics.length; i++) {
+      var a = $('<button class="btn btn-primary">');
+      a.attr("id", "show");
+      a.attr("data-search", topics[i]);
+      a.text(topics[i]);
+      $("#myButtons").append(a);
+    }
+  }
+
+
+  displayButtons();
+
+  
+  $(document).on("click", "#show", displayPokemon);
+
+  $(document).on("click", ".pokemonGiphy", pausePlayGifs);
+
+  function pausePlayGifs() {
+  	 var state = $(this).attr("data-state");
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+  }
+}
+
+});
